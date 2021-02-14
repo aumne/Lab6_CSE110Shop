8b878c14869b404ea9dd303e1d7d358ddc08a5de
aumne <50233978+aumne@users.noreply.github.com>
2021-02-13 23:56:58 -0800

    rewrite fetch code

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,11 @@
 window.addEventListener('DOMContentLoaded', () => {
   // fetch data if not stored yet
   if (!(localStorage.getItem('fetch_data'))) { 
-    const response = await fetch('https://fakestoreapi.com/products');
-    const fetch_data = await response.json();
-    localStorage.setItem('fetch_data', JSON.stringify(fetch_data));
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => {
+        localStorage.setItem('fetch_data', JSON.stringify(data));
+      });
   }
 
   if (!(localStorage.getItem('cart_count'))) { 
